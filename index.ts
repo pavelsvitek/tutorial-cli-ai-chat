4e195be44c56e0346d51413267097881a028b215
Pavel Svitek <svitekpavel@gmail.com>
2025-03-03 19:09:25 +0100

    implement messages

--- index.ts
+++ index.ts
@@ -1,21 +1,25 @@
 import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
+import { CoreMessage, generateText } from "ai";
 
 const model = openai("gpt-4o-mini");
-const systemPrompt = `Respond as a pirate.
-- Use pirate slang and grammar
-- Use pirate emojis`;
+const systemPrompt = `Respond as a pirate.`;
 
 export async function generateAnswer(prompt: string) {
-  const { text } = await generateText({
-    model,
-    prompt,
-    system: systemPrompt,
-  });
+  const messages: CoreMessage[] = [
+    {
+      role: 'system',
+      content: systemPrompt,
+    },
+    // ..all previous messages
+    {
+      role: 'user',
+      content: prompt,
+    }
+  ]
 
+  const { text } = await generateText({ model, messages });
   return text;
 }
 
 const answer = await generateAnswer("Who is Sam Altman?");
-
 console.log(answer);
